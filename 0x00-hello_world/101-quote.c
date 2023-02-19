68b9f767e760dc2cdb25f9844534690c574e2e1f
adane <adanemoges6@gmail.com>
2023-02-19 22:58:18 +0300

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	Write(2,"and that piece of art os useful\" - Dora korpar, 2015-10-19\n",61)
+
+	return (1);
+}
