cc08c5f94fadfa3aa6ccd1c5314dd4016c1e85f6
adane <adanemoges6@gmail.com>
2023-02-21 01:54:36 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
