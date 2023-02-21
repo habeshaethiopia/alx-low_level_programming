4bc3f390e0860b53d8737af93b13f9f46e52f339
adane <adanemoges6@gmail.com>
2023-02-21 04:59:47 +0300

    advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,15 @@ int main(void)
 
 	for (i = 0; i < 90; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i != 89)
+		if (i / 10 != i % 10)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
