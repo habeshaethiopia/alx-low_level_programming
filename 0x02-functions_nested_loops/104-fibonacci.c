d0ecbbc303f1d5bc8246b120766d5432c487cb4e
adane <adanemoges6@gmail.com>
2023-02-22 00:46:44 +0300

    100 - table

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,11 +17,26 @@ int main(void)
 
 	printf("%lu", bef);
 
-	for (i = 1; i < 98; i++)
+	for (i = 1; i < 9; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
+	}
+	unsigned long int l =1000000000;
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
