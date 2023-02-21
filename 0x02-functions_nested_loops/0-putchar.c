c21b27e2887510a8bac1fafe87e89e42044d8314
adane <adanemoges6@gmail.com>
2023-02-21 22:39:20 +0300

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	printf("_putchar/n");
+	return (0);
+}
