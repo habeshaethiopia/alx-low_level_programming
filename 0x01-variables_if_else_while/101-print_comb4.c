519dec8a24300fa3ff403d47af11ce49e865857c
adane <adanemoges6@gmail.com>
2023-02-21 05:39:55 +0300

    last adva

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
