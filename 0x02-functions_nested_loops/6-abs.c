5a0b8a890ee04e47b8e6f983b316c7dff6f9cfa4
adane <adanemoges6@gmail.com>
2023-02-23 12:53:39 +0300

    task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - is afunction used to done ansolute value function
+ * @n: int parameter
+ * Return: the changed value n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1)
+	else
+		return (n);
+}
