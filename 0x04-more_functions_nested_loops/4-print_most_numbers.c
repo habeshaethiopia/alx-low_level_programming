72e207fb2057072f299184386bae7c5b63e5d84f
adane <adanemoges6@gmail.com>
2023-02-24 01:04:55 +0300

    execlude 2 and 4 from the list

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - The function
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
