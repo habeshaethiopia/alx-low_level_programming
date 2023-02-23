42b59f92047608d3a4bf32a5f98a179d95d308ab
adane <adanemoges6@gmail.com>
2023-02-23 15:07:03 +0300

    time in 24hr

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Is afunction that prints every miniuts of a day
+ *
+ * Return: void nothing it returns
+ */
+void jack_bauer(void)
+{
+	int w = 0, x = 0, y = 0, z = 0;
+	int m = 10;
+
+	for (w = 0; w < 3; w++)
+	{
+		if (w == 2)
+			m = 3;
+		for (x = 0; x < m + 1; x++)
+		{
+			for (y = 0; y < 6; y++)
+			{
+				for (z = 0; z < 10; z++)
+				{
+					_putchar(w + '0');
+					_putchar(x + '0');
+					_putchar(':');
+					_putchar(y + '0');
+					_putchar(z + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
