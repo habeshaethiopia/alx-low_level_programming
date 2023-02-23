3423255f89a17bb934f3f4d84e999ce0a13f9d28
adane <adanemoges6@gmail.com>
2023-02-24 02:11:28 +0300

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,25 +5,25 @@
 void more_numbers(void)
 {
 	int x, y;
-	int i, j;
+	int i = '0', j = '0';
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 14; y++)
-		{
-			i = '0';
-			j = '0';
-
-			if (i > '9')
-				i = 1;
-			_putchar(i);
-			i++;
-			if (y > 9)
+			for (y = 0; y <= 14; y++)
+			{
+				if (i > '9')
+					i = '1';
+				_putchar(i);
+				if (y <= 9)
+				i++;
+				if (y > 9)
 			{
 				_putchar(j);
 				j++;
 			}
 		}
+		i = '0';
+		j = '0';
 		_putchar('\n');
 	}
 }
