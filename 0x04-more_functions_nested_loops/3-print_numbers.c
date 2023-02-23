a01d5f74c3ef4c1cb5bb663916c567e2926dedcc
adane <adanemoges6@gmail.com>
2023-02-24 01:00:07 +0300

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - The function
+ *
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
