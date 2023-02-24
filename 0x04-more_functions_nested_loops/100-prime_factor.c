9ee5a9ff5e3fdbbcc61f950b920b9f96d9d71c87
adane <adanemoges6@gmail.com>
2023-02-24 04:30:24 +0300

    advanced one

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+	long int i = 612852475143;
+
+	while (i != 1)
+	{
+		for (x = 2; x <= i;)
+		{
+			if (i % x == 0)
+			{
+				printf("%d, ", x);
+				i = i / x;
+			}
+			else
+				x++;
+		}
+	}
+	printf("\n");
+}
