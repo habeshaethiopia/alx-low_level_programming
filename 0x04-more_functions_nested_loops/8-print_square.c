5a11c5226442887f3c6b4d7a523d1b1744569ed8
adane <adanemoges6@gmail.com>
2023-02-24 03:33:34 +0300

    last mandatory task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - is the function
+ * @size: the size of the square
+ *
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
