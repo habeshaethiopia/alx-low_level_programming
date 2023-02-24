23213f000096cfced9217602dead3f7b81c0fd0b
adane <adanemoges6@gmail.com>
2023-02-24 05:21:33 +0300

    hope

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,13 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0 sucess.
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	i = 0;
+	x = 0;
 	positive_or_negative(i);
 
 	return (0);
