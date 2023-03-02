5f33e9788e7947b435f783e5ef279fa633d6b1d9
adane <adanemoges6@gmail.com>
2023-03-02 13:35:26 +0300

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ * _strcat - a Function that do the same as strcat
+ * @dest: string
+ * @src: appended string to dest
+ * Return: dest string (char pointer)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j]
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
