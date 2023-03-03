a0ec803b4ca2c4573ed3ee3e8dc9cc19bc48ab1d
adane <adanemoges6@gmail.com>
2023-03-03 19:39:20 +0300

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,9 +13,13 @@ char *cap_string(char *s)
 	while (s[i] != '\0')
 	{
 		for (x = 0; x < 12; x++)
-		if (s[i] == separator[x])
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s[i + 1] = s[i + 1] - 32;
+			if (s[i] == separator[x])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+				if (s[i] == '\t')
+					s[i] = ' ';
+			}
 		i++;
 	}
 	return (s);
