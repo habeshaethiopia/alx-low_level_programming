828663cf08954b947bde77cab29338a6eca7e1b0
adane <adanemoges6@gmail.com>
2023-03-03 20:34:35 +0300

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,21 @@
 /**
+ * is_spa - function to determine if the char is separator or not
+ * @x: acharacter
+ * return: integer 0 or 1
+ *
+ */
+int is_spa(char x)
+{
+	int separator[] = {',', ';', '.', '?', '"', '(', ')',
+		'{', '}', ' ', '\n', '\t'};
+	int i;
+
+	for (i = 0; i < 12; i++)
+		if (x == separator[i])
+			return (1);
+	return (0);
+}
+/**
  * cap_string - afunction to capitalize aword
  * @s: astring made of words
  * Return: a char pointer
@@ -6,18 +23,16 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, x;
-	int separator[] = {',', ';', '.', '?', '"', '(', ')',
-		'{', '}', ' ', '\n', '\t'};
+	int i = 0, x = 1;
 
 	while (s[i] != '\0')
 	{
-		for (x = 0; x < 12; x++)
-			if (s[i] == separator[x])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] = s[i + 1] - 32;
-			}
+		if (s[i] >= 'a' && s[i] <= 'z' && x == 1)
+		{
+			s[i] = s[i] - 32;
+			x = 0;
+		}
+		x = is_spa(s[i]);
 		i++;
 	}
 	return (s);
