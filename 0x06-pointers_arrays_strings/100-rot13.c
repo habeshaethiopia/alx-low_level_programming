0cb1b8591261a24f0ee4864da73e2021f35681b3
adane <adanemoges6@gmail.com>
2023-03-04 00:24:36 +0300

    encoding using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,14 +6,14 @@
  */
 char *rot13(char *str)
 {
-	int i = 0, j;
+	int i = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i]<= 'm'))
+		 for (; ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i]<= 'Z')); i++)
+			 str[i] -= 13;
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i]<= 'M'))
 			str[i]  += 13;
-		for (j = 0; ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i]<= 'Z')); j++)
-			str[i] -= 13;
 	}
 	return (str);
 }
