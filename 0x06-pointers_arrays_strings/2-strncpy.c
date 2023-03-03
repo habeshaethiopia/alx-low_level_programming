e19c0947f8c62930939537ced97f708f62144fe3
adane <adanemoges6@gmail.com>
2023-03-03 19:57:53 +0300

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		i++;
+		for (; i < n; i++)
+			dest[i] = '*';
 	}
-	for (; i < n; i++)
-		dest[i] = '*';
 	return (dest);
 }
