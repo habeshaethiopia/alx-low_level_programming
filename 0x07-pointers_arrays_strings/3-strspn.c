333021cd4c55271e1e860ed7ab095de753f1203e
adane <adanemoges6@gmail.com>
2023-03-07 02:43:51 +0300

    unsigned int _strspn(char *s, char *accept)

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\n'; i++)
 		for (j = 0; accept[j] != '\n'; j++)
-			if (s[i] != accept[j])
+			if (s[i] == accept[j])
 				n++;
 	return (n);
 }
