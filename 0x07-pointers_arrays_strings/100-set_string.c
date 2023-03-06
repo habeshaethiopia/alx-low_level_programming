a4070c0477d49c2c3e70e8d289df07c2855566a4
adane <adanemoges6@gmail.com>
2023-03-07 00:08:51 +0300

    dive into advanced task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,13 @@
 /**
- * set_string - function to set pointer to a char
- * @s: apointer of pointer
- * @to: char pointer
+ * set_string - function
+ * @s: double pointer
+ * @to: pointer
  *
  */
 void set_string(char **s, char *to)
 {
-	*s = to;
+	char **x;
+
+	x = s;
+	*x = to;
 }
