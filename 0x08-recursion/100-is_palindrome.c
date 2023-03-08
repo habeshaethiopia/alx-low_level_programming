6ccc3bbe4481a7df7c7582af90211718b150d26b
adane <adanemoges6@gmail.com>
2023-03-09 01:38:21 +0300

    addvanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,43 @@
-char *_rev(char *str)
-{
-	char *rev = "";
-
-	if (*str != '\0')
-		rev = _rev(str++);
-	return rev;
-}
-int cmpwrev(char *Or, char *rev)
-{
-	int x = 1;
-
-	if (*Or == *rev && *Or != '\0')
-		x *= cmpwrev(Or++, rev++);
-	else if (*Or != '\0' && *rev != '\0')
-		return (0);
-	return (x);
-}
-
+#include <stdio.h>
+#include "main.h"
 /**
- * is_palindrome - function to check palindrome
- * @s: string
- * Return: 0 or 1
- *
+ * _strlen_recursion - returns the length of a string.
+ * @s: the string to count
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	return (0);
+}
+/**
+ * checker - helper function for is_palindrome
+ * @str: the string
+ * @len: length of string
+ * @count: counter of recursion
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+int checker(char *str, int len, int count)
+{
+	if (count >= len)
+		return (1);
+	if (str[len] == str[count])
+		return (checker(str, len - 1, count + 1));
+	return (0);
+}
+/**
+ * is_palindrome - checks if the string is a palindrome
+ * @s: the string to check
+ * Return: 1 if string is a palindrome, 0 if it is not.
  */
 int is_palindrome(char *s)
 {
-	char * rev = _rev(s);
+	int len = _strlen_recursion(s);
+	int count = 0;
 
-	return (cmpwrev(s, rev));
+	return (checker(s, len - 1, count));
 }
