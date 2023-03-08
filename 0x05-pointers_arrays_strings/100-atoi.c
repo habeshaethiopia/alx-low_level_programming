ebf675425314dc2fe70fb8b587e654c9d1bc6cf0
adane <adanemoges6@gmail.com>
2023-03-08 05:22:13 +0300

    after all

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdbool.h>
 #include "main.h"
 /**
  * _atoi - function
@@ -8,5 +8,25 @@
  */
 int _atoi(char *s)
 {
+	int i, minus, val;
+	bool ch = false;
 
+	minus = -1;
+	val = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] ==  '-')
+			minus *= -1;
+		if (s[i] >= '0' && s[i] <= 9)
+		{
+			val *= 10;
+			val -= s[i] - '0';
+			ch = true;
+		}
+		if (ch)
+			break;
+	}
+	val -= minus;
+	return (val);
 }
+
