1dd2bd3edaa007b11054766f856a33f834db2b41
adane <adanemoges6@gmail.com>
2023-03-08 03:41:39 +0300

    update strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,8 +10,12 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 
 	for (i = 0; s[i] != '\n'; i++)
+	{
 		for (j = 0; accept[j] != '\n'; j++)
 			if (s[i] != accept[j])
 				n++;
-	return (n);
+		if (i == n)
+			break;
+	}
+	return (i);
 }
