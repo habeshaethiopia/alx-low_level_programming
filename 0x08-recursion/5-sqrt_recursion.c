ad4056e6e2139002010a3e09cc753cfb18ce6fb5
adane <adanemoges6@gmail.com>
2023-03-10 01:12:44 +0300

    second month alx

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,20 @@
-int square (int n, int sq)
+/**
+ * square - function that do the comparision
+ * @n: the original number
+ * @sq: the variable
+ * Return: the sq if sq^2 == num else return -1
+ *
+ */
+int square(int n, int sq)
 {
 	if (n == sq * sq)
 		return (sq);
 	else if (n < sq * sq)
 		return (-1);
-	else 
+	else
 		return (square(n, sq + 1));
 }
-				
+
 /**
  * _sqrt_recursion - a function that returns the squre root of a num
  * @n: number
@@ -19,5 +26,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (square(n, 0)); 
+		return (square(n, 0));
 }
