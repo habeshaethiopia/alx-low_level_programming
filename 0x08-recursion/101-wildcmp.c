0614f1a6f202df660ba0171bf1007a4394ef4b35
adane <adanemoges6@gmail.com>
2023-03-10 01:45:27 +0300

    new level of understanding

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,21 +5,18 @@
  * Return: 0 od 1
  *
  */
-int wildcmp(char* s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
-	if ((*s2 == '*' && *(s2 + 1) == '\0'))
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s2 == '*' && *(s2 + 1) != '*' && *s1 != '\0')	
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == *s1 && *(s2 + 1) != '\0')
+		if ((*(s2 + 1) == '*'))
 			return (wildcmp(s1, s2 + 1));
-		else
-			return (wildcmp(s1 + 1, s2));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
 	}
-	 if ((*s2 == '*' && *(s2 + 1) == '*' && *s1 != '\0') ||(*s2 == *s1 && *(s1 + 1) != '\0'))
-		return (wildcmp(s1 + 1,s2 + 1));
-	 if (*s1 == *s2 && *(s1 + 1) == '\0' && *(s2 + 1) == '\0')
-		return (1);
-	 else
-		return (0);
+	return (0);
 }
