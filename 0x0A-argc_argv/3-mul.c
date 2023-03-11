358360d6253f009cf584893f9c4219d80f7bbd51
adane <adanemoges6@gmail.com>
2023-03-11 18:03:21 +0300

    this about argc & argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 3 || argc == 1)
+	int mul;
+
+	if (argc < 3 )
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (argc == 3)
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
 	else
-		printf("%d\n", atoi(argv[1]));
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
 	return (0);
 }
