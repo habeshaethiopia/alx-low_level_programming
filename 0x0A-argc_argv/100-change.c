23dd93c19e098ad6299101cbad47341e67dea525
adane <adanemoges6@gmail.com>
2023-03-13 00:54:44 +0300

    this is all

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum amount of coin
+ * @argc: counter
+ * @argv: string array
+ * Return: 0(Success) or 1(fail)
+ */
+int main(int argc, char *argv[])
+{
+	int bir;
+	int cent = 0;
+	if (argc !=2)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	bir = atoi(argv[1]); 
+	while (bir >= 25)
+	{
+		cent++;
+		bir -= 25;
+	}
+	while (bir >= 10)
+	{
+		cent++;
+		bir -= 10;
+	}
+	while (bir >= 5)
+	{
+		cent++;
+		bir -= 5;
+	}
+	while (bir >= 2)
+	{
+		cent++;
+		bir -= 2;
+	}
+	while (bir > 0)
+	{
+		cent++;
+		bir -= 1;
+	}
+        if (bir == 0)
+		printf("%d\n", cent);
+
+
+	return (0);
+}
