eb804e9a75d30b5d805f8e5b5d6159744621c214
adane <adanemoges6@gmail.com>
2023-03-14 15:29:41 +0300

    add strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,11 @@ char *str_concat(char *s1, char *s2)
 	s1 == NULL ? s1 = "" : 0;
 	s2 == NULL ? s2 = "" : 0;
 	for (i = 0; s1[i] != '\0'; i++)
-	 {
-	 }
+	{
+	}
 	for (j = 0; s2[j] != '\0'; j++)
-	 {
-	 }
+	{
+	}
 	p = malloc(i + j + 1);
 	if (p == NULL)
 		return (NULL);
