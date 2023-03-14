d100b18832b27206e13729c3c49862475456ab21
adane <adanemoges6@gmail.com>
2023-03-14 15:06:24 +0300

    add two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * str_concat - concatinate two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: concatinated string
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i, j;
+
+	for (i = 0, j = 0; s1[i] != '\0' || s2[j] != '\0'; i++, j++)
+	{
+	}
+	p = malloc(i + j + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		p[i + j] = s2[j];
+	p[i + j] = '\0';
+	return (p);
+}
+
+
