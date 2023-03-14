8c4f6b6b084a401393cedde695ed1139d895ec38
adane <adanemoges6@gmail.com>
2023-03-14 17:45:53 +0300

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,6 +14,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	p = malloc(height * sizeof(int *));
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		p[i] = malloc(width * sizeof(int));
