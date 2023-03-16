9a09e6c69c66e63b770ed1a2a4bfe0c63d450705
adane <adanemoges6@gmail.com>
2023-03-16 23:30:19 +0300

    1-stringnconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,12 +13,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	if (s1 == NULL)
-		return (s2);
+		s2 = "";
 	if (s2 == NULL)
-		return (s1);
+		s1 = "";
 	for (i = 0; s2[i] != '\0'; i++)
 		c++;
-	if (n <= c)
+	if (n < c)
 		c = n;
 	for (i = 0; s1[i] != '\0'; i++)
 		c++;
