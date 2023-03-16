51b24aa3c1601254bd3a744131b524e04ae4662c
adane <adanemoges6@gmail.com>
2023-03-16 23:14:36 +0300

    100 - realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 /**
  * _realloc - reallocates a memory block
- * @ptr: void pointer
+ * @pt: void pointer
  * @old_size: the allocated space for ptr
  * @new_size: the new size for new memory block
  * Return: void pointer
