4e439cb15e979d3ba41d23d652ff6882c531dcf3
adane <adanemoges6@gmail.com>
2023-03-16 23:11:49 +0300

    100 - realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,6 +7,27 @@
  * Return: void pointer
  *
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *pt, unsigned int old_size, unsigned int new_size)
 {
+	char *p, *ptr;
+	int min = old_size, i;
+
+	ptr = pt;
+	if (new_size ==  0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size < old_size)
+		min = new_size;
+	for (i = 0; i < min; i++)
+		p[i] = ptr[i];
+	free(ptr);
+	return ((void *) p);
+}
 
