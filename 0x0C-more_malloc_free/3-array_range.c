71844b4ef587bf10e358e2beff0be07a81b4bf52
adane <adanemoges6@gmail.com>
2023-03-16 23:52:20 +0300

    3-array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,11 +9,15 @@
 int *array_range(int min, int max)
 {
 	int *p;
+	int i;
 
 	if (min > max)
 		return (NULL);
-	p = malloc(sizeof(int) * (max - min) + 1);
+	p = malloc(sizeof(int) * (max - min + 1));
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; min <= max ; min++, i++)
+		p[i] = min;
+
 	return (p);
 }
