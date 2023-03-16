e0e98e8f4b4fbbe5adb395331c32fb2796a59ba3
adane <adanemoges6@gmail.com>
2023-03-16 18:10:23 +0300

    task 0 and task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,6 +10,5 @@ void *malloc_checked(unsigned int b)
 {
 	if (malloc(b) != NULL)
 		return (malloc(b));
-	else
-		exit(98);
+	exit(98);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+/**
+ * string_nconcat - concatinate two string
+ * @s1: string one
+ * @s2: string two
+ * @n: number of concatinated strings
+ * Return: the concatinated string
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int j, i, c = 0;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		return (s1);
+	for (i = 0; s2[i] != '\0'; i++)
+		c++;
+	if (n <= c)
+		c = n;
+	for (i = 0; s1[i] != '\0'; i++)
+		c++;
+	p = malloc(c * sizeof(char) + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		p[i] = s1[i];
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+	return (p);
+}
