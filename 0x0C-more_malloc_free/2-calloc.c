2158b073450a834c28aaf27d0f60f62551d886a8
adane <adanemoges6@gmail.com>
2023-03-16 21:53:48 +0300

    void *_calloc(unsigned int nmemb, unsigned int size)

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * _calloc - function like calloc
+ * @nmemb: no elements
+ * @size: biyte of each array element
+ * Return: void pointer
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (malloc(nmemb * size) == NULL)
+		return (NULL);
+	return (malloc(nmemb * size));
+}
+
