d382ac71e611e407ef42ac3c558bc96a929e09ee
adane <adanemoges6@gmail.com>
2023-03-20 23:51:47 +0300

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - function to creat new dog
+ * @name: dogs name
+ * @age: age of the new dog
+ * @owner: Owner's name
+ * Return: dog_t the new data type
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p = malloc(sizeof(dog_t));
+
+	if (p == NULL)
+		return (NULL);
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return(NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->owner = owner;
+	p->age = age;
+	return (p);
+}
