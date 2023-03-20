255845da64a928395e71657e62827e0bc7913353
adane <adanemoges6@gmail.com>
2023-03-20 22:24:58 +0300

    first user define data type in c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the new data type name
+ * @name: char pointer
+ * @age: float type
+ * @owner: char pointer (type string)
+ * description : this is structure of new data type called dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
