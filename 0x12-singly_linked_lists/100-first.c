5867fe9c3de5c49ad7583e5b741fe3b56f359d1b
adane <adanemoges6@gmail.com>
2023-04-01 01:09:25 +0300

    advanced 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * premain - execute before main
  *
  */
-void _attribute_ ((constructor)) premain(void)
+void __attribute__ ((constructor)) premain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
