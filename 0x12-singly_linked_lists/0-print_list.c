fb0d266c5914353ee53c51ed15d49e80f07848e3
adane <adanemoges6@gmail.com>
2023-03-31 22:28:18 +0300

    project staarted

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,12 @@ size_t print_list(const list_t *h)
 {
 	size_t l = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len, h->str);
 		l++;
 		h = h->next;
 	}
