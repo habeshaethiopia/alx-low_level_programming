72f82d3244d6b449ea1fb1f19ee6d001589815b6
adane <adanemoges6@gmail.com>
2023-04-01 00:11:14 +0300

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add end of the node
+ * @head: the list head
+ * @str: the value
+ * Return: the pointer to the new element
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *s;
+	list_t *new = malloc(sizeof(list_t)), *p;
+
+	if (!str || !new)
+		return (NULL);
+	s = strdup(str);
+	if (!s)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(s);
+	new->str = s;
+	new->next = NULL;
+	p = *head;
+	if (!*head)
+	{
+		*head = new;
+		return (new);
+	}
+	while (p->next)
+		p = p->next;
+
+	p->next = new;
+
+	return (new);
+}
+/**
+ * _strlen - this functions prints the lenght of a string
+ (* a blank line
+ *@s: string to print.
+ (* section header: the header of this function is holberton.h)*
+ * Return: retunrn the lenght in int
+ */
+int _strlen(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (1 + _strlen(s));
+	}
+	else
+		return (0);
+}
