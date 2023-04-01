55931cfec88e17a44fa64ea6adaf6eface153161
adane <adanemoges6@gmail.com>
2023-04-01 03:09:02 +0300

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t l = 0;
+	int l = 0;
 
-	while (h)
+	for (l = 0; h ; l++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%i] %s\n", h->len, h->str);
-		l++;
 		h = h->next;
 	}
 	return (l);
