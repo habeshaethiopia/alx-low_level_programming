d472849cd373cf9c490adf946c8c32521fdb00c7
adane <adanemoges6@gmail.com>
2023-04-03 12:45:55 +0300

    this is the first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t
+ * @h: apointer to the head of the the list
+ * Return: the size of the poiter
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	if (!h)
+		return (0);
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0x13-more_singly_linked_lists directory is open in 25 መጋቢት 2015 ዓ.ም 
