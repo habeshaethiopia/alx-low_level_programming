6c3e5b93d6a672166ce2a8d6fd2edb526c187c97
adane <adanemoges6@gmail.com>
2023-04-05 01:01:06 +0300

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,8 @@
  * Return: the pointer for the new none
  *
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
 	listint_t *new = malloc(sizeof(listint_t));
 	unsigned int i;
 	listint_t *p;
