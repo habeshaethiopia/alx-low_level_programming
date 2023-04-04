8038cc2ef54bfa7d9afe0fb345c11ea9359ab11d
adane <adanemoges6@gmail.com>
2023-04-05 01:48:43 +0300

    adding the reverse list program

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse the list
+ * @head: the head of the array
+ * Return: pointer to the revarsed list head
+ *
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p1, *p2;
+
+	if (!head || *head)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	p1 = *head;
+	p2 = p1->next;
+	*head = p2->next;
+	p1->next = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		p2->next = p1;
+		p1 = p2;
+		p2 = *head;
+		*head = (*head)->next;
+	}
+	(*head)->next = p2;
+	p2->next = p1;
+	return (*head);
+}
