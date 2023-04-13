f782b125067ef45645943a13f0294c9f699ecd9e
adane <adanemoges6@gmail.com>
2023-04-13 15:28:49 +0300

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file -  append atext at the end
+ * @filename: the name of the file
+ * @text_content: the appened text
+ * Return: 1 or -1
+ *
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fd;
+	int i, l = 0;
+	char *buff = malloc(sizeof(char) * 1024);
+
+	fd = fopen(filename, "a");
+	if (!fd)
+		return (-1);
+	while (text_content[l])
+	{
+		l++;
+	}
+	i = fwrite(buff, 1, l, fd);
+	if (i == -1)
+		return (-1);
+	fclose(fd);
+	return (1);
+}
