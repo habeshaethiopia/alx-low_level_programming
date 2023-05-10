bb53e786972fc71cd5c6b401c86c62547ccdfd51
adane <adanemoges6@gmail.com>
2023-05-10 21:31:47 +0300

    forget betty on main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,14 @@
-#ifndef MAIN_H
-#define MATH_H
+#ifndef _FILE_MAIN
+#define _FILE_MAIN
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-int _putchar(char s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char s);
 
 #endif
