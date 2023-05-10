55abde850655ec203dbddefcf00a7d39355d0251
adane <adanemoges6@gmail.com>
2023-05-10 21:37:45 +0300

    updating task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	dprintf(fd, "%s", text_content);
+	if (text_content)
+		dprintf(fd, "%s", text_content);
+
 	return (1);
 }
 
