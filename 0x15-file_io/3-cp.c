2ce0718d87a9a1aae11cf65f8f521a496d9e8f4e
adane <adanemoges6@gmail.com>
2023-05-16 22:25:08 +0300

    this is the last

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,9 +8,10 @@
  */
 int main(int ac ,char **av)
 {
-	int x, y;
+	size_t x, y;
 	FILE *or, *cp;
 	char *buff = malloc(sizeof(char) * 1024);
+	size_t n  = 0;
 	if (!buff)
 		return(0);
 	if (ac != 3)
@@ -26,31 +27,25 @@ int main(int ac ,char **av)
 		free(buff);
 		exit(98);
 	}
-	cp = fopen(av[2], "w");
+	cp = fopen(av[2], "w+");
 	if (!cp)
 	{
 		dprintf(2,"Error: Can't write to %s \n", av[2]);
 		free(buff);
 		exit(99);
 	}
-	while((x = fread(buff, 1024, 1, or)) > 0)
+	while((x = getline(&buff, &n, or)) != -1)
 	{
-		y = fwrite(buff, x, 1, cp);
-		if (!cp || x != y)
-		{
-			dprintf(2,"Error: Can't write to %s \n", av[2]);
-			free(buff);
-			exit(99);
-		}
+		y = fwrite(buff, n, 1, cp);
 	}
 
 
 	x = fclose(or);
 	if (x != 0)
-		dprintf(2,"Error: Can't close fd %d", x);
+		dprintf(2,"Error: Can't close fd %lu", x);
 	y = fclose(cp);
 	if (y != 0)
-		dprintf(2,"Error: Can't close fd %d", y);
+		dprintf(2,"Error: Can't close fd %lu", y);
 
 	return (0);
 }
