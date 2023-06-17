7f186a986ae03bfd3202920c6598c3d5035e5482
Adane Moges <adanemogs6@gmail.com>
2023-06-17 14:14:50 +0300

    betty corecting

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
+
 /**
- * Print_dlistint - the function
- *
+ * print_dlistint - the function that prints the node
  * @h: apointer to the head of the list
  * Return: size_t
+ *
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	int n = 0;
