926dbb5361b51f04c9523082ec94eaf2991932b1
Adane Moges <adanemogs6@gmail.com>
2023-06-19 22:05:11 +0300

    lert me see it

---
+++ 0x18-dynamic_libraries/fun5.c
@@ -0,0 +1,9 @@
+unsigned int _strspn(char *s, char *accept)
+{
+}
+char *_strpbrk(char *s, char *accept)
+{
+}
+char *_strstr(char *haystack, char *needle)
+{
+}
---
+++ 0x18-dynamic_libraries/func2.c
@@ -0,0 +1,12 @@
+int _isdigit(int c)
+{
+}
+int _strlen(char *s)
+{
+}
+void _puts(char *s)
+{
+}
+char *_strcpy(char *dest, char *src)
+{
+}
