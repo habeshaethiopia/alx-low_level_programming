9b20a3c9fdd357acfab55483d018532376e678b8
Adane Moges <adanemogs6@gmail.com>
2023-06-19 23:21:32 +0300

    advance 1

---
+++ 0x18-dynamic_libraries/fun6.c
@@ -0,0 +1,56 @@
+/**
+ * add - fun
+ *
+ * @b: var
+ * @a: var
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - fun
+ *
+ * @a: var
+ * @b: var
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - fun
+ *
+ * @a: var
+ * @b: var
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - fun
+ *
+ * @a: var
+ * @b: var
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - fun
+ *
+ * @a: var
+ * @b: var
+ * Return: int
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
