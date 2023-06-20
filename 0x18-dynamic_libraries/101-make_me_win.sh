3a25b056b644b9bf96702f97de6f1bb5c07deb93
Adane Moges <adanemogs6@gmail.com>
2023-06-20 11:00:23 +0300

    not today

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gcc --shared -o /tmp/librand.so mylib.c 
  LD_PRELOAD=/tmp/librand.so
+export LD_PRELOAD=/tmp/librand.so
--- 0x18-dynamic_libraries/mylib.c
+++ 0x18-dynamic_libraries/mylib.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
+#include <string.h>
 
-static int index = 0;
-static int values[] = {9, 8, 10, 24, 75, 9};
-static int num_values = 6;
+int rand(void)
+{
+    static int i = 0;
+    static int values[] = {9, 8, 10, 24, 75, 9};
+    static int num_values = 6;
 
-int rand(void) {
-    int value = values[index];
-    index = (index + 1) % num_values;
+    int value = values[i];
+    i = (i + 1) % num_values;
     return value;
 }
