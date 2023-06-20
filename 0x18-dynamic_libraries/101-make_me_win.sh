2124494c2186e7536828d97dda9770cfc37ce9ca
Adane Moges <adanemogs6@gmail.com>
2023-06-20 11:07:11 +0300

    not today

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc --shared -o /tmp/librand.so mylib.c 
-export LD_PRELOAD=/tmp/librand.so
+wget https://github.com/habeshaethiopia/alx-low_level_programming/raw/3a25b056b644b9bf96702f97de6f1bb5c07deb93/0x18-dynamic_libraries/librand.so
+LD_PRELOAD=/tmp/librand.so
