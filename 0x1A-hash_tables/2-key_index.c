05fe5802b6a1b79e6ad3d3ccf85372de5d4f802a
Adane Moges <adanemogs6@gmail.com>
2023-07-07 00:23:49 +0300

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,15 @@
 #include "hash_tables.h"
+/**
+ * key_index - afunction to find the approprate index of the key
+ * 
+ * @key: the Key
+ * @size:the value
+ * Return: unsigned long int 
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int index;
+
+    index = hash_djb2(key) % size;
+    return (index);
+}
