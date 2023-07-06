a935fbc73ef2c6d419dda2b2c10241f8b0fc5495
Adane Moges <adanemogs6@gmail.com>
2023-07-07 02:41:51 +0300

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,1 +1,33 @@
 #include "hash_tables.h"
+/**
+ * hash_table_delete- afunction to clean all ths space allocated for hashing
+ *
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr, *temp;
+
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				curr = ht->array[i];
+				while (curr)
+				{
+					temp = curr;
+					free(curr->key);
+					if (curr->value)
+						free(curr->value);
+					curr = curr->next;
+					free(temp);
+				}
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
