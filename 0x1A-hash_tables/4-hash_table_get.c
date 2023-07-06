2b256ed9b7bdc7c96ca148d001bd23d1ca418581
Adane Moges <adanemogs6@gmail.com>
2023-07-07 02:06:18 +0300

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,25 @@
 #include "hash_tables.h"
+/**
+ * hash_table_get - function to retive the data
+ *
+ * @ht:the hash table
+ * @key:the key
+ * Return: char*
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int i;
+	hash_node_t *curr;
+
+	if (!ht || !key)
+		return (NULL);
+	i = key_index(key, ht->size);
+	curr = ht->array[i];
+	while (curr)
+	{
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+		curr = curr->next;
+	}
+	return (NULL);
+}
