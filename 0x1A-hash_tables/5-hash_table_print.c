895a8b9c01ff4cc40f5cb39853b7cce3468ae25f
Adane Moges <adanemogs6@gmail.com>
2023-07-07 02:29:46 +0300

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,32 @@
 #include "hash_tables.h"
+/**
+ * hash_table_print - function to print the overall table of the hash
+ *
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr;
+	int flag = 0;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				curr = ht->array[i];
+				while (curr)
+				{
+					flag == 1 ? printf(", ") : flag;
+					printf("'%s': '%s'", curr->key, curr->value);
+					flag = 1;
+					curr = curr->next;
+				}
+			}
+		}
+		printf("}\n");
+	}
+}
