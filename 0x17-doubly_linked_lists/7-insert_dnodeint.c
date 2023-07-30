8f0d140ff15ad648da66300973304c00b1a0740c
Adane Moges <adanemogs6@gmail.com>
2023-07-31 00:02:45 +0300

    task 8

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index- the function to insert the element
+ *
+ * @h: the head
+ * @idx: the  index
+ * @n: the value
+ * Return: dlistint_t*
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *temp = *h;
+	unsigned int i;
+
+	if (h == NULL)
+		return (NULL);
+	for (i = 0; i < idx; i++)
+	{
+		temp = temp->next;
+		if (!temp)
+			return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (temp != NULL)
+	{
+		new->next = temp->next;
+		if (temp->next->prev)
+			temp->next->prev = new;
+		temp->next = new;
+		new->prev = temp;
+	}
+	else
+		*h = new;
+	return (new);
+}
