b5af77ee05e470c054d2aefbcaa5458829e9e994
Adane Moges <adanemogs6@gmail.com>
2023-07-30 20:05:00 +0300

    try

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,13 +6,14 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-
+	dlistint_t *temp;
 	if (!head)
 		return;
 	while (head)
 	{
+		temp = head;
+		free(temp);
 		head = head->next;
-		free(head->prev);
 	}
 	head = NULL;
 }
