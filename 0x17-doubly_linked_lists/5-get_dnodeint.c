f07e132217033a8891e0b3a364f91beb7b72a9cb
Adane Moges <adanemogs6@gmail.com>
2023-07-30 20:22:30 +0300

    the sixth task

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * @brief Get the dnodeint at index object - function
+ * get_dnodeint_at_index - Get the dnodeint at index object
  *
  * @head:the arguments to the the function
  * @index:the arguments to the the function
@@ -8,13 +8,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < index && head; i++, head = head->next)
-    {
-    }
-    if (!head)
-        return (NULL);
-    else
-        return (head);
+	for (i = 0; i < index && head; i++, head = head->next)
+	{
+	}
+	if (!head)
+		return (NULL);
+	else
+		return (head);
 }
