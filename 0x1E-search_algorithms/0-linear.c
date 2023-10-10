8986589e57df3598da45e190a2189bc18b9f11e0
Adane <adanemoges6@gmail.com>
2023-10-10 18:31:28 +0300

    first git

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ * linear_search - search using linear search
+ * @array: pointer to the array
+ * @size: length of the array
+ * @value: the searched
+ * Return: the index or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%d]\n", i, array[i]); 
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef search
+#define search
+#include <stddef.h>
+#include "stdio.h"
+#include "stdlib.h"
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
